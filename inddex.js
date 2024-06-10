2d5de4a196dd9fb7575c5d78110355e2a6ae9ca3
Srinath <srinathkmr0031@gmail.com>
2024-06-10 23:38:32 +0530

    1st commit

---
+++ inddex.js
@@ -0,0 +1,28 @@
+const containerEl=document.querySelector(".container");
+for(let i=0;i<30;i++){
+    const colorContainerEl=document.createElement("div");
+    colorContainerEl.classList.add("color-container");
+    containerEl.appendChild(colorContainerEl);
+}
+
+const colorContainerEls=document.querySelectorAll(".color-container");
+
+generateColors()
+function generateColors(){
+    colorContainerEls.forEach((colorContainerEl)=>{
+        const newColorCode=randomColor();
+        colorContainerEl.style.backgroundColor="#"+newColorCode;
+        colorContainerEl.innerText="#" + newColorCode;
+    });
+}
+
+function randomColor(){
+    const chars="0123456789abcdef";
+    const colorCodeLength=6;
+    let colorCode="";
+    for(let i=0;i<colorCodeLength;i++){
+        const randNo=Math.floor(Math.random()*chars.length);
+        colorCode+=chars.substring(randNo,randNo+1);
+    }
+    return colorCode;
+}
